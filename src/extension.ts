4e333d87dc6fec2013d568bbe45b992625356b7c
Adrian Kiesthardt <adrian@kiesthardt.com>
2024-09-05 00:38:58 +0200

    Removed unnecessary code and dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import * as mdItContainer from "markdown-it-container";
-import { blockName, scriptPlugin } from "./markdown-it/scriptPlugin";
 import path from "path";
 import * as fs from "fs";
 
@@ -22,9 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   logger.clear();
-  logger.appendLine("Congrats");
   logger.show(true);
-  logger.appendLine("Hello World");
 
   const loadScriptCommand = vscode.commands.registerCommand(
     "markdown-scripts.reloadScripts",
@@ -78,56 +74,11 @@ export function activate(context: vscode.ExtensionContext) {
       });
     }
   );
-
-  // Execute the command once when the extension is activated
-  // vscode.commands.executeCommand("markdown-scripts.reloadScripts");
-
   context.subscriptions.push(loadScriptCommand);
 
   // register Markdown It plugin
-  return {
-    extendMarkdownIt(md: markdownit) {
-      extendMarkdownItWithScripts(md, logger, {
-        languageIds: () => {
-          return [];
-          // return vscode.workspace
-          //   .getConfiguration("markdown-scripts") // ! keep in sync with name from package.json > configuration
-          //   .get<string[]>("languages", ["js-exec"]);
-        },
-      });
-      return md;
-    },
-  };
+  return {};
 }
-
-// // Function to get the existing markdown preview webview
-// function getMarkdownPreviewWebview(
-//   uri: vscode.Uri
-// ): vscode.WebviewPanel | undefined {
-//   // VS Code's API to retrieve the existing markdown preview panel might need to be adapted
-//   const panels = vscode.window.visibleTextEditors.filter(
-//     (editor) => editor.document.uri.toString() === uri.toString()
-//   );
-//   if (panels.length > 0) {
-//     const webviewPanel = panels[0] as vscode.WebviewPanel;
-//     return webviewPanel;
-//   }
-//   return undefined;
-// }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
-
-function extendMarkdownItWithScripts(
-  md: markdownit,
-  logger: vscode.LogOutputChannel,
-  config?: { languageIds(): readonly string[] }
-) {
-  // TODO: extract markdown-it parser into own project for separate testing
-  // TODO: create test runner script for markdown-it plugin
-  // TODO: because test runner wants to download VSCode
-  // TODO: --> create docker-image for testing
-  md.use(scriptPlugin, logger);
-
-  return md;
-}
